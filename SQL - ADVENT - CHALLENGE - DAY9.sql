0f4cc83fb8e9980a8e30a9cb50af00bfaf34e5b9
Harish-Thulasiraman <harishthulasiraman12@gmail.com>
2026-01-08 12:03:41 +0530

    Create SQL - ADVENT - CHALLENGE - DAY9.sql

---
+++ SQL - ADVENT - CHALLENGE - DAY9.sql
@@ -0,0 +1,31 @@
+-- SQL Advent Calendar - Day 9
+-- Title: Tinsel and Light Combinations
+-- Difficulty: hard
+--
+-- Question:
+-- The elves are testing new tinsel–light combinations to find the next big holiday trend. Write a query to generate every possible pairing of tinsel colors and light colors, include in your output a column that combines the two values separated with a dash ("-").
+--
+-- The elves are testing new tinsel–light combinations to find the next big holiday trend. Write a query to generate every possible pairing of tinsel colors and light colors, include in your output a column that combines the two values separated with a dash ("-"). 
+--
+
+-- Table Schema:
+-- Table: tinsel_colors
+--   tinsel_id: INT
+--   color_name: VARCHAR
+--
+-- Table: light_colors
+--   light_id: INT
+--   color_name: VARCHAR
+--
+
+-- My Solution:
+PostgreSQL
+--SWFGHOL
+SELECT tinsel_colors.color_name, 
+       light_colors.color_name,
+       CONCAT(tinsel_colors.color_name, '-',light_colors.color_name ) AS result
+FROM tinsel_colors
+CROSS JOIN light_colors 
+
+
+--CROSS JOIN doesnt work with same columns using ON
